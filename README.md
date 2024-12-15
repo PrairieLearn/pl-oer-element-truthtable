0395df78a82304c55e03d7f668cf9e2d0b408f07
Nico Ritschel <nico@elsl.de>
2024-12-14 20:11:06 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,19 +1,40 @@
-## PrairieLearn Course Repository
+# PrairieLearn OER Element: Truth Table
 
-### Welcome to PrairieLearn! 
+This element was developed by Pai Zheng and Yujie Miao. Please carefully test the element and understand its features and limitations before deploying it in a course. It is provided as-is and not officially maintained by PrairieLearn, so we can only provide limited support for any issues you encounter!
 
-The content for your course is stored within this repository.
-You can see and edit a live version at [https://prairielearn.com](https://prairielearn.com)
+If you like this element, you can use it in your own PrairieLearn course by copying the contents of the `elements` folder into your own course repository. After syncing, the element can be used as illustrated by the example question that is also contained in this repository.
 
-### Getting Started
 
-Learn how to create your first questions and assessments using our [Get Started](https://prairielearn.readthedocs.io/en/latest/getStarted/) tutorial.
+## `pl-truth-table` element
 
-### Getting Help
+This element creates a truth table and can be used for both instructional materials and questions. Input columns are auto-generated to enumerate all boolean combinations. Output columns are supplied as an attribute (either to be displayed or to be used for grading student inputs). The table supports both partial and all-or-nothing grading, different input alphabets (e.g., 0/1 or T/F), and custom bit widths (e.g., "011" as a single output column).
 
-Need help understanding an error or creating content? Check out one of the following resources:
+### Example
 
-1. Real-time help assistance on PrairieLearn's [Slack `#pl-help` channel](https://prairielearn.com/slack).
-1. Looking up the question on our Frequently Asked Question (FAQ) pages: [readthedocs](https://prairielearn.readthedocs.io/en/latest/faq/) and [github-discussions](https://github.com/PrairieLearn/PrairieLearn/discussions/categories/q-a)
-1. Attend virtual office hours, which are announced at the start of the
-   semester on PrairieLearn's [Slack](https://prairielearn.com/slack)
+![truth_table](example.png)
+
+    ```html
+    <pl-truth-table
+        answers-name="q2"
+        input-name="[X, Y, Z]"
+        output-name="X AND Y AND Z"
+        output-values="[0,0,0,0,0,0,0,1]"
+    </pl-truth-table>>
+    ```
+
+### Element Attributes
+
+| Attribute | Type | Description |
+|-----------|------|-------------|
+| `answers-name` | string (required) | Unique name for the element. |
+| `input-name` | string (required) | Names of the input variables, displayed in the table header. For multiple input columns, wrap names in the style of an array (in square brakets and comma-seperated, e.g., `"[X, Y, Z]"`). |
+| `output-name` | string (required) | Names of the input variables, displayed in the table header. For multiple input columns, wrap names in the style of an array (in square brakets and comma-seperated, `"[X OR Y, X AND Y]"`). |
+| `bit-width` | string (default: `"1"`) | The expected number of bits for each output variable. Input the number as a string. |
+| `is-material` | boolean (default: `false`) | If set to `true`, outputs are displayed and the table is static, to be used as instructor-provided material |
+| `output-values` | string (required) | Output values for the table (displayed to students when `is-material=true`, and used for grading otherwise). Wrap values in the style of an array (in square brakets and comma-seperated, e.g., `"[0, 1, 1, 1]"`). For multiple output columns, separate each column's values by a comma (e.g., `"[0, 1, 1, 1], [0, 0, 0, 1]"`). The number of output columns must match the number of names provided in `output-name`, and the number of values per column must be 2^N for N input columns. |
+| `input-alphabet` | string (default: `10`) | The characters used to represent true and false. Set `input-alphabet="TF"` to use T and F, or `input-alphabet="tf"` to use t and f. Note that the alphabet used in `output-values` has to match the one set here. |
+| `placeholder` | string (default: `?`) | The placeholder shown for empty input boxes. |
+| `prefill` | string (default: `0`) | A value prefilled for all input boxes (useful for large but sparsely filled tables). |
+| `show-partial-score` | boolean (default: `true`) | Shows row-by-row grading feedback via a badge next to the each row. |
+| `show-percentage-score` | boolean (default: `true`) | Assigns a percentage score for the question that is displayed as a badge. If set to `false`, all-or-nothing grading is used. |
+
---
+++ elements/pl-truth-table/pl-truth-table.py
@@ -0,0 +1,452 @@
+import random
+import re
+from enum import Enum
+from typing import Any
+
+import chevron
+import lxml.html
+import prairielearn as pl
+from typing_extensions import assert_never
+
+BIT_WIDTH_DEFAULT = "1"
+CORRECT_ANSWER_DEFAULT = None
+INPUT_ALPHABET_DEFAULT = "10"
+IS_MATERIAL_DEFAULT = False
+LABEL_DEFAULT = None
+PLACEHOLDER_DEFAULT = "?"
+PREFILL_DEFAULT = "0"
+SHOW_HELP_TEXT_DEFAULT = True
+SHOW_PERCENTAGE_SCORE_DEFAULT = True
+SHOW_PARTIAL_SCORE_DEFAULT = True
+
+TRUTH_TABLE_MUSTACHE_TEMPLATE_NAME = "pl-truth-table.mustache"
+
+
+def prepare(element_html: str, data: pl.QuestionData) -> None:
+    element = lxml.html.fragment_fromstring(element_html)
+    required_attribs = ["answers-name", "input-name", "output-name", "output-values"]
+    optional_attribs = [
+        "label",
+        "display",
+        "prefill",
+        "placeholder",
+        "show-partial-score",
+        "is-material",
+        "show-percentage-score",
+        "bit-width",
+        "input-alphabet",
+    ]
+    pl.check_attribs(element, required_attribs, optional_attribs)
+
+    name = pl.get_string_attrib(element, "answers-name")
+    pl.check_answers_names(data, name)
+
+    output_name = pl.get_string_attrib(element, "output-name")  # [X or Y, X and Y]
+    output_name = output_name.lstrip("[").rstrip("]").split(",")
+
+    # prepare the input variables e.g. ['X', 'Y']
+    variable_string = pl.get_string_attrib(element, "input-name")
+    variables = variable_string.strip("[").strip("]").split(",")
+    # check if different bitwidth is provided for each variable
+    bit_width = pl.get_string_attrib(element, "bit-width", BIT_WIDTH_DEFAULT)
+    num_rows = 1
+    if "," in bit_width:
+        bit_width = bit_width.lstrip("[").rstrip("]").split(",")
+        for x in bit_width:
+            num_rows *= 2 ** int(x)
+    else:
+        num_rows = 2 ** (
+            int(bit_width) * len(variables)
+        )  # Total rows in the truth table based on the number of variables and bit width
+    # Get the single correct-answer string from the HTML and split it for each row
+    output_string = pl.get_string_attrib(
+        element, "output-values", CORRECT_ANSWER_DEFAULT
+    )
+    output_list = (
+        re.split(r"\],\s+\[", output_string) if output_string is not None else None
+    )
+    # check if the number of output lists is equal to the number of output names
+    if output_list is not None:
+        if len(output_list) != len(output_name):
+            raise ValueError(
+                f"The number of output lists ({len(output_list)}) must match the number of output names ({len(output_name)})."
+            )
+    for k, output in enumerate(output_list):
+        output = output.lstrip("[").rstrip("]").replace(" ", "").split(",")
+        if output is not None:
+            if len(output) != num_rows:
+                raise ValueError(
+                    f"The length of the correct answer ({len(output)}) must match the number of rows ({num_rows})."
+                )
+            # check if the value is in the input alphabet
+            input_alphabet = pl.get_string_attrib(
+                element, "input-alphabet", INPUT_ALPHABET_DEFAULT
+            )
+            char_level_set = {char for item in set(output) for char in item}
+            if not char_level_set.issubset(set(input_alphabet)):
+                raise ValueError(
+                    f"Invalid format. Provided output {char_level_set} not in alphabet {set(input_alphabet)}."
+                )
+            width = len(output[0])
+            # Split the correct_answer into individual answers for each row
+            for row_index in range(num_rows):
+                if len(output[row_index]) != width:
+                    raise ValueError(
+                        f"The bit-width inside correct answer list {k} is not consistent. Please check."
+                    )
+                data["correct_answers"][f"{name}_{row_index}_{k}"] = output[row_index]
+
+def render(element_html: str, data: pl.QuestionData) -> str:
+    element = lxml.html.fragment_fromstring(element_html)
+    name = pl.get_string_attrib(element, "answers-name")
+    label = pl.get_string_attrib(element, "label", LABEL_DEFAULT)
+    # Get the output name
+    output_name = pl.get_string_attrib(element, "output-name")  # [X or Y, X and Y]
+    output_name = output_name.lstrip("[").rstrip("]").split(",")
+    for o_name in output_name:
+        o_name = o_name.lstrip("").rstrip("")
+    num_output = len(output_name)
+
+    # Determine if the question is material (informational) or requires input
+    is_material = pl.get_boolean_attrib(
+        element, "is-material", False
+    )  # Default to False if not specified
+    placeholder = pl.get_string_attrib(element, "placeholder", PLACEHOLDER_DEFAULT)
+    placeholder_l = []
+    for k in range(num_output):
+        placeholder_l.append(placeholder * len(data["correct_answers"][f"{name}_0_{k}"]))
+    prefill = pl.get_string_attrib(element, "prefill", PREFILL_DEFAULT)
+    show_percentage_score = pl.get_boolean_attrib(element, "show-percentage-score", SHOW_PERCENTAGE_SCORE_DEFAULT)
+    show_partial_score = pl.get_boolean_attrib(
+        element, "show-partial-score", SHOW_PARTIAL_SCORE_DEFAULT
+    )
+    score = data["partial_scores"].get(name, {"score": None}).get("score", None)
+    if score is not None:
+        score = float(score) * 100
+    ac = score == 100
+    aw = score == 0
+
+    # prepare the input variables e.g. ['X', 'Y']
+    variable_string = pl.get_string_attrib(element, "input-name")
+    variables = variable_string.strip("[").strip("]").split(",")
+
+    # Generate table data
+    var_lenth = len(variables)
+    # check if different bitwidth is provided for each variable
+    bit_width = pl.get_string_attrib(element, "bit-width", BIT_WIDTH_DEFAULT)
+    num_rows = 1
+    if "," in bit_width:
+        bit_width_list = bit_width.lstrip("[").rstrip("]").split(",")
+        for x in bit_width_list:
+            num_rows *= 2 ** int(x)
+    else:
+        num_rows = 2 ** (
+            int(bit_width) * var_lenth
+        )  # Total rows in the truth table based on the number of variables and bit width
+    columns = [{"name": c} for c in variables]
+    rows = []
+    for i in range(num_rows):
+        row = {
+            "input": [],
+            "row_index": i,
+            "name": name,
+            "is_first_row": i == 0,
+            "output": [],
+        }
+        if "," in bit_width:
+            max_values = [2 ** int(w) for w in bit_width_list]
+            counts = []
+            rem = i
+            # Convert the count into counts for each bitwidth
+            for mv in reversed(max_values):
+                counts.append(rem % mv)
+                rem //= mv
+            counts.reverse()
+            # Generate the binary strings for the current counts
+            row["input"] = [format(counts[i], f'0{int(bit_width_list[i])}b') for i in range(len(bit_width_list))]
+        else:
+            for j in range(var_lenth):
+                # Calculate the bit position for the j-th variable
+                bit_position = (var_lenth - j - 1) * int(bit_width)
+                # Extract bit_width bits for the j-th variable from the row index
+                # Shift right to bring the desired bits to the least significant position
+                # Then apply a mask to extract exactly k bits
+                bits = (i >> bit_position) & ((1 << int(bit_width)) - 1)
+                # Format the bits as a binary string with leading zeros to ensure k bits
+                bit_str = format(bits, f"0{bit_width}b")
+                row["input"].append(bit_str)
+        for k in range(num_output):
+            output = {
+                "cell_name": f"{name}_{i}_{k}",
+                "output_index": k,
+                "sub": data["submitted_answers"].get(f"{name}_{i}_{k}", prefill * len(data["correct_answers"][f"{name}_{i}_{k}"])),
+                "correct": False,
+                "incorrect": False,
+                "input_error": data["format_errors"].get(f"{name}_{i}_{k}", None),
+                "output_value": data["correct_answers"].get(
+                    f"{name}_{i}_{k}", CORRECT_ANSWER_DEFAULT
+                ),
+                "placeholder": placeholder_l[k],
+            }
+            row["output"].append(output)
+        rows.append(row)
+    for index in range(num_rows):
+        for k in range(num_output):
+            answer_name = f"{name}_{index}_{k}"
+            partial_score = (
+                data["partial_scores"]
+                .get(answer_name, {"score": None})
+                .get("score", None)
+            )
+            if partial_score is not None:
+                try:
+                    partial_score = float(partial_score)
+                    if partial_score >= 1:
+                        rows[index]["output"][k]["correct"] = True
+                    else:
+                        rows[index]["output"][k]["incorrect"] = True
+                except Exception as e:
+                    raise ValueError("invalid score" + partial_score) from e
+    # Get template
+    with open(TRUTH_TABLE_MUSTACHE_TEMPLATE_NAME, "r", encoding="utf-8") as f:
+        template = f.read()
+    if data["panel"] == "question":
+        grading_text = ""
+        if show_partial_score:
+            if show_percentage_score:
+                grading_text = "Feedback for each cell is available."  
+        else:
+            if show_percentage_score:
+                grading_text = "Feedback for each cell is hidden. Partial Score is shown above."  
+            else:
+                grading_text = "Partial score and feedback for each cell is hidden. You need to get all cells correct to get full score. Partial correct will still result in 0%."
+
+        info_params = {
+            "format": True,
+            "grading_text": grading_text,
+            "input_alphabet": str(
+                set(
+                    pl.get_string_attrib(
+                        element, "input-alphabet", INPUT_ALPHABET_DEFAULT
+                    )
+                )
+            ),
+        }
+        info = chevron.render(template, info_params).strip()
+        html_params = {
+            "question": True,
+            "name": name,
+            "output_name": output_name,
+            "label": label,
+            "info": info,
+            "uuid": pl.get_uuid(),
+            "columns": columns,
+            "rows": rows,
+            "num_rows": num_rows,
+            "is_material": is_material,
+            "show_partial_score": show_partial_score,
+            "score": score,
+            "all_correct": ac,
+            "all_incorrect": aw,
+        }
+        return chevron.render(template, html_params).strip()
+    elif data["panel"] == "submission":
+        html_params = {
+            "submission": True,
+            "name": name,
+            "output_name": output_name,
+            "label": label,
+            "uuid": pl.get_uuid(),
+            "columns": columns,
+            "rows": rows,
+            "num_rows": num_rows,
+            "show_partial_score": show_partial_score,
+            "score": score,
+            "all_correct": ac,
+            "all_incorrect": aw,
+        }
+        for index in range(num_rows):
+            for k in range(num_output):
+                # Construct the indexed name (e.g., answers-name_0, answers-name_1, ...)
+                answer_name = f"{name}_{index}_{k}"
+                parse_error = data["format_errors"].get(answer_name, None)
+                if parse_error is None and answer_name in data["submitted_answers"]:
+                    # Get submitted answer, raising an exception if it does not exist
+                    a_sub = data["submitted_answers"].get(answer_name, None)
+                    if a_sub is None:
+                        raise Exception("submitted answer is None")
+                    # If answer is in a format generated by pl.to_json, convert it
+                    # back to a standard type (otherwise, do nothing)
+                    a_sub = pl.from_json(a_sub)
+                    a_sub = pl.escape_unicode_string(a_sub)
+                    html_params["a_sub"] = a_sub
+                elif answer_name not in data["submitted_answers"]:
+                    data["format_errors"][answer_name] = "No submitted answer."
+                    html_params["parse_error"] = None
+        if show_partial_score and score is not None:
+            score_type, score_value = pl.determine_score_params(score)
+            html_params[score_type] = score_value
+        return chevron.render(template, html_params).strip()
+    elif data["panel"] == "answer":
+        html_params = {
+            "answer": True,
+            "name": name,
+            "output_name": output_name,
+            "label": label,
+            "columns": columns,
+            "rows": rows,
+        }
+        return chevron.render(template, html_params).strip()
+
+def parse(element_html: str, data: pl.QuestionData) -> None:
+    element = lxml.html.fragment_fromstring(element_html)
+    name = pl.get_string_attrib(element, "answers-name")
+    # Get the output name
+    output_name = pl.get_string_attrib(element, "output-name")  # [X or Y, X and Y]
+    output_name = output_name.lstrip("[").rstrip("]").split(",")
+    for o_name in output_name:
+        o_name = o_name.lstrip("").rstrip("")
+    num_output = len(output_name)
+
+    # Check if the question is marked as material (informational)
+    is_material = pl.get_boolean_attrib(element, "is-material", False)
+    # If it's material, skip grading
+    if is_material:
+        return
+    
+    # prepare the input variables e.g. ['X', 'Y']
+    variable_string = pl.get_string_attrib(element, "input-name")
+    variables = variable_string.strip("[").strip("]").split(",")
+
+    # check if different bitwidth is provided for each variable
+    bit_width = pl.get_string_attrib(element, "bit-width", BIT_WIDTH_DEFAULT)
+    num_rows = 1
+    if "," in bit_width:
+        bit_width_l = bit_width.lstrip("[").rstrip("]").split(",")
+        for x in bit_width_l:
+            num_rows *= 2 ** int(x)
+    else:
+        num_rows = 2 ** (
+            int(bit_width) * len(variables)
+        )  # Total rows in the truth table based on the number of variables and bit width
+        
+    input_alphabet = pl.get_string_attrib(
+        element, "input-alphabet", INPUT_ALPHABET_DEFAULT
+    )
+
+    # Loop through each row to capture submitted answers
+    for row_index in range(num_rows):
+        for k in range(num_output):
+            answer_name = f"{name}_{row_index}_{k}"
+            a_sub = data["submitted_answers"].get(answer_name, None)
+            expected_len = len(data["correct_answers"][answer_name])
+            if a_sub is None:
+                data["format_errors"][answer_name] = "No submitted answer."
+                data["submitted_answers"][answer_name] = None
+                continue
+            if not a_sub:
+                data["format_errors"][
+                    answer_name
+                ] = "Invalid format. The submitted answer was left blank."
+                data["submitted_answers"][answer_name] = None
+            elif len(a_sub) != expected_len:
+                data["format_errors"][
+                    answer_name
+                ] = f"Invalid format. The submitted answer must be {expected_len} bit(s) long."
+                data["submitted_answers"][answer_name] = None
+            elif not set(a_sub).issubset(set(input_alphabet)):
+                data["format_errors"][
+                    answer_name
+                ] = f"Invalid format. Input not in alphabet {set(input_alphabet)}."
+                data["submitted_answers"][answer_name] = None
+            else:
+                # Store the submitted answer for this row
+                data["submitted_answers"][answer_name] = pl.to_json(a_sub)
+
+def grade(element_html: str, data: pl.QuestionData) -> None:
+    element = lxml.html.fragment_fromstring(element_html)
+
+    # Check if the question is marked as material (informational)
+    is_material = pl.get_boolean_attrib(element, "is-material", False)
+    # If it's material, skip grading
+    if is_material:
+        return
+
+    name = pl.get_string_attrib(element, "answers-name")
+    # prepare the input variables e.g. ['X', 'Y']
+    variable_string = pl.get_string_attrib(element, "input-name")
+    variables = variable_string.strip("[").strip("]").split(",")
+    # check if different bitwidth is provided for each variable
+    bit_width = pl.get_string_attrib(element, "bit-width", BIT_WIDTH_DEFAULT)
+    num_rows = 1
+    if "," in bit_width:
+        bit_width = bit_width.lstrip("[").rstrip("]").split(",")
+        for x in bit_width:
+            num_rows *= 2 ** int(x)
+    else:
+        num_rows = 2 ** (
+            int(bit_width) * len(variables)
+        )  # Total rows in the truth table based on the number of variables and bit width
+    # Get the output name
+    output_name = pl.get_string_attrib(element, "output-name")  # [X or Y, X and Y]
+    output_name = output_name.lstrip("[").rstrip("]").split(",")
+    for o_name in output_name:
+        o_name = o_name.lstrip("").rstrip("")
+    num_output = len(output_name)
+
+    percentage_score = pl.get_boolean_attrib(
+        element, "show-percentage-score", SHOW_PERCENTAGE_SCORE_DEFAULT
+    )
+
+    is_incorrect = False
+    score_sum = 0
+    for index in range(num_rows):
+        for k in range(num_output):
+            answer_name = f"{name}_{index}_{k}"
+            # Get the correct answer for each row
+            a_tru = pl.from_json(data["correct_answers"].get(answer_name, None))
+            if a_tru is None:
+                break
+            if answer_name in data["submitted_answers"]:
+                if data["submitted_answers"][answer_name] == a_tru:
+                    data["partial_scores"][answer_name] = {
+                        "score": 1,
+                        "feedback": "Correct.",
+                    }
+                    score_sum += 1
+                else:
+                    data["partial_scores"][answer_name] = {
+                        "score": 0,
+                        "feedback": "Incorrect.",
+                    }
+                    if not percentage_score:
+                        is_incorrect = True
+                        break
+            else:
+                data["partial_scores"][answer_name] = {
+                    "score": 0,
+                    "feedback": "Missing input.",
+                }
+                if not percentage_score:
+                    is_incorrect = True
+                    break
+
+    if not percentage_score and is_incorrect:
+        # assign the score to all the answers
+        score_sum = 0
+        for index in range(num_rows):
+            for k in range(num_output):
+                answer_name = f"{name}_{index}_{k}"
+                a_tru = pl.from_json(data["correct_answers"].get(answer_name, None))
+                if a_tru is None:
+                    break
+                data["partial_scores"][answer_name] = {
+                    "score": 0.0,
+                    "feedback": "",
+                }
+
+    data["partial_scores"][name] = {"score": score_sum / (num_rows * num_output)}
+
+
+def test(element_html: str, data: pl.ElementTestData) -> None:
+    return
